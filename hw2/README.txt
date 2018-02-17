7ba1ec8cf7928dce8824ca1690260a5a09069dd8
senyuuri <senyuuri@gmail.com>
2018-02-17 23:04:17 +0800

    Add query parser

--- hw2/README.txt
+++ hw2/README.txt
@@ -1,11 +1,17 @@
-This is the README file for A0000000X's submission
+This is the README file for A0144315N's submission
 
 == Python Version ==
 
-I'm (We're) using Python Version <2.7.x or replace version number> for
+I'm using Python Version 2.7.12 for
 this assignment.
 
 == General Notes about this assignment ==
+
+
+python index.py -i /mnt/c/Users/senyu/workspace/cs3245-information-retrieval/nltk_data/corpora/reuters/training/ -d dictionary.txt -p postings.txt
+
+
+
 
 Give an overview of your program, describe the important algorithms/steps 
 in your program, and discuss your experiments in general.  A few paragraphs 
@@ -21,16 +27,11 @@ and formatted correctly.
 
 Please initial one of the following statements.
 
-[ ] I, A0000000X, certify that I have followed the CS 3245 Information
+I, A0144315N, certify that I have followed the CS 3245 Information
 Retrieval class guidelines for homework assignments.  In particular, I
 expressly vow that I have followed the Facebook rule in discussing
 with others in doing the assignment and did not take notes (digital or
 printed) from the discussions.  
-
-[ ] I, A0000000X, did not follow the class rules regarding homework
-assignment, because of the following reason:
-
-<Please fill in>
 
 I suggest that I should be graded as follows:
 
@@ -40,3 +41,12 @@ I suggest that I should be graded as follows:
 
 <Please list any websites and/or people you consulted with for this
 assignment and state their role>
+
+[1]NLTK Howto: 3.Processing Raw Text
+http://www.nltk.org/book/ch03.html
+
+[2]How sent_tokenizer + word_tokenizer work together
+https://stackoverflow.com/questions/33773157/word-tokenize-typeerror-expected-string-or-buffer
+
+[3]Shunting-yard Algorithm
+https://en.wikipedia.org/wiki/Shunting-yard_algorithm
--- hw2/index.py
+++ hw2/index.py
@@ -2,10 +2,54 @@
 import re
 import nltk
 import sys
+import glob
 import getopt
+import time
 
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
+
+def punc_normalise(sent):
+    """Normalise punctuations"""
+    token = token.replace("/",' ')
+
+def parse_query(raw):
+    """Use shunting-yard algorithm to convert query into Reverse Polish notation(RPN)"""
+    # operator keywords in the order of increasing precedence
+    OPWORDS = ['OR','AND','NOT']
+    ORDER = OPWORDS + ['('+')']
+    # operator stack
+    op = []
+    # output queue
+    output = []
+    raw = raw.replace('(',' ( ').replace(')',' ) ').split(' ')
+    for r in raw:
+        print(r)
+        if r=='(':
+            op.append(r)
+        elif r==')':
+            top = op.pop()
+            while top != '(':
+                output.append(top)
+                top = op.pop()
+            op.append(top)
+        elif r not in OPWORDS:
+            output.append(r)
+        else:
+            if len(op) != 0:
+                top = op.pop()
+                while top != '(' and len(op) > 0 and OPWORDS.index(top) >= OPWORDS.index(r):
+                    output.append(top)
+                    top = op.pop()
+                op.append(top)
+            op.append(r)
+
+    while(len(op) != 0):
+        output.append(op.pop())
+
+    return output
+
+
 
 input_directory = output_file_dictionary = output_file_postings = None
 
@@ -28,3 +72,36 @@ for o, a in opts:
 if input_directory == None or output_file_postings == None or output_file_dictionary == None:
     usage()
     sys.exit(2)
+
+start_time = time.time()
+# in-memory temporary posting list
+p = {} 
+# using porter stemmer
+porter = nltk.PorterStemmer()
+# get the list of files inside the input directory
+files = glob.glob(input_directory + "*")
+fcount = 0
+for f in files:
+    docID = f.split('/')[-1]
+    # TESTING ONLY
+    fcount += 1
+    if fcount > 100:
+        break
+
+    with open(f, 'r') as fopen:
+        for sent in nltk.sent_tokenize(fopen.read()):
+            tokens = nltk.word_tokenize(sent)
+            for t in tokens:
+                t = porter.stem(t.lower()).encode("ascii")
+                # create a new dictionary entry if t is a new keyword
+                if t not in p:
+                    p[t] = [docID]
+                else:
+                    # add docID to posting list if it is not inside
+                    if docID not in p[t]:
+                        p[t].append(docID) 
+    # print(p)
+
+print(str(fcount)+" records processed in "+str(time.time() - start_time)+" seconds.")
+query = "bill OR Gates AND (vista OR XP) AND NOT mac"
+print(parse_query(query))
