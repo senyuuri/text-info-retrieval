f77fc1d69bcd2db5859a5862408eaf4114189bb4
senyuuri <senyuuri@gmail.com>
2018-02-18 11:51:37 +0800

    Add boolean expression parser

--- hw2/index.py
+++ hw2/index.py
@@ -9,12 +9,81 @@ import time
 # operator keywords in the order of increasing precedence
 OPWORDS = ['OR','AND','NOT']
 
+# TODO metadata to be read from the 1st line of dictionary.txt
+# range of docIDs used for indexing
+DOCID_MIN = 1
+DOCID_MAX = 10158
+
+
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
+
 
 def punc_normalise(sent):
     """Normalise punctuations"""
     token = token.replace("/",' ')
+
+
+def get_posting(keyword):
+    """Get the posting list by keyword"""
+    # TODO implement disk-based postings indexing
+    if keyword in p:
+        return p[keyword]
+    else:
+        return []
+
+
+def evaluate_NOT(p):
+    """Evaluate NOT operation on a postings list"""
+    result = []
+    for i in range(DOCID_MIN, DOCID_MAX+1):
+        if str(i) not in p:
+            result.append(str(i))
+    print('evaluated NOT: ' + str(len(result)) + ' results')
+    return result
+
+def evaluate_AND(p1, p2):
+    """Evaluate AND operation on a postings list"""
+    #TODO use skip pointer
+    if len(p1) == 0 or len(p2) == 0:
+        return []
+    else:
+        result = []
+        while len(p1) != 0 and len(p2) != 0:
+            if p1[0] == p2[0]:
+                result.append(p1[0])
+                p1.pop(0)
+                p2.pop(0)
+            elif p1[0] < p2[0]:
+                p1.pop(0)
+            else:
+                p2.pop(0)
+        print('evaluated AND: ' + str(len(result)) + ' results')
+        print(result)
+        print('===================')
+        return result
+
+def evaluate_OR(p1, p2):
+    """Evaluate OR operation on a postings list"""
+    # TODO use skip pointer
+    if len(p1) == 0:
+        return p2
+    elif len(p2) == 0:
+        return p1
+    else:
+        result = []
+        while len(p1) != 0 and len(p2) != 0:
+            if p1[0] == p2[0]:
+                p1.pop(0)
+                p2.pop(0)
+            elif p1[0] < p2[0]:
+                result.append(p1.pop(0))
+            else:
+                result.append(p2.pop(0))
+        print('evaluated OR: ' + str(len(result)) + ' results')
+        print(result)
+        print('===================')
+        return result
 
 def parse_query(raw):
     """Use shunting-yard algorithm to convert query into Reverse Polish notation(RPN)
@@ -60,11 +129,11 @@ def parse_query(raw):
 
     return evaluate_query(output)
 
+
 def evaluate_query(qlist):
     """Evaluate a boolean query in the form of Reverse Polish notation
 
     Query is evaluated from left to right.
-    For intermediate result, a special operator "RESULT" will be pushed into the stack.
 
     Input:
         qlist: a list of operators and operands in RPN order
@@ -79,17 +148,22 @@ def evaluate_query(qlist):
         if q in OPWORDS:
             if q == 'NOT':
                 op1 = stack.pop()
-                # print('NOT '+op1)
-                # stack.append('NOT '+op1)
+                # if op1 is a list: it is an intermediate result from previosu steps
+                # else: op1 is a keyword, retrieve corresponding postings list
+                print('plist:'+str(plist))
+                plist = op1 if type(op1)==list else get_posting(op1)
+                stack.append(evaluate_NOT(plist))
             else:
                 op1 = stack.pop()
                 op2 = stack.pop()
+                plist1 = op1 if type(op1)==list else get_posting(op1)
+                plist2 = op2 if type(op2)==list else get_posting(op2)
+                print('plist1:'+str(plist1))
+                print('plist2:'+str(plist2))
                 if q == 'AND':
-                    
+                    stack.append(evaluate_AND(plist1, plist2))
                 elif q == 'OR':
-                    continue
-                # print(op1+' '+q+' '+op2)
-                # stack.append('('+op1+' '+q+' '+op2+')')
+                    stack.append(evaluate_OR(plist1, plist2))
         else:
             stack.append(q)
     return stack.pop()
