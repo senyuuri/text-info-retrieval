e2e5aada0a0d5d22ad5c0175655be840209847c2
senyuuri <senyuuri@gmail.com>
2018-03-08 02:45:09 +0800

    Add test files; Fix boolean evaluation logic

--- hw2/index.py
+++ hw2/index.py
@@ -27,20 +27,36 @@ def punc_normalise(sent):
 def get_posting(keyword):
     """Get the posting list by keyword"""
     # TODO implement disk-based postings indexing
-    if keyword in p:
-        return p[keyword]
+    if keyword in postings:
+        return postings[keyword]
     else:
         return []
 
 
-def evaluate_NOT(p):
+def evaluate_NOT(q):
     """Evaluate NOT operation on a postings list"""
-    result = []
-    for i in range(DOCID_MIN, DOCID_MAX+1):
-        if str(i) not in p:
-            result.append(str(i))
-    print('evaluated NOT: ' + str(len(result)) + ' results')
-    return result
+    if len(q) == 0:
+        return doclist
+    else:
+        result = []
+        tmp_doclist = doclist
+        while len(tmp_doclist) != 0 and len(q) != 0:
+            if tmp_doclist[0] == q[0]:
+                q.pop(0)
+                tmp_doclist.pop(0)
+            elif tmp_doclist[0] < q[0]:
+                result.append(tmp_doclist.pop(0))
+            else:
+                result.append(q.pop(0))
+
+        # pop remaining items in doclist
+        while len(tmp_doclist) != 0:
+            result.append(tmp_doclist.pop(0))
+        print('evaluated NOT: ' + str(len(result)) + ' results')
+        print(result)
+        print('===================')
+        return result
+
 
 def evaluate_AND(p1, p2):
     """Evaluate AND operation on a postings list"""
@@ -51,8 +67,7 @@ def evaluate_AND(p1, p2):
         result = []
         while len(p1) != 0 and len(p2) != 0:
             if p1[0] == p2[0]:
-                result.append(p1[0])
-                p1.pop(0)
+                result.append(p1.pop(0))
                 p2.pop(0)
             elif p1[0] < p2[0]:
                 p1.pop(0)
@@ -62,6 +77,7 @@ def evaluate_AND(p1, p2):
         print(result)
         print('===================')
         return result
+
 
 def evaluate_OR(p1, p2):
     """Evaluate OR operation on a postings list"""
@@ -74,12 +90,19 @@ def evaluate_OR(p1, p2):
         result = []
         while len(p1) != 0 and len(p2) != 0:
             if p1[0] == p2[0]:
-                p1.pop(0)
+                result.append(p1.pop(0))
                 p2.pop(0)
             elif p1[0] < p2[0]:
                 result.append(p1.pop(0))
             else:
                 result.append(p2.pop(0))
+
+        # add all remaining docIDs to result
+        while len(p1) != 0:
+            result.append(p1.pop(0))
+        while len(p2) != 0:
+            result.append(p2.pop(0))
+
         print('evaluated OR: ' + str(len(result)) + ' results')
         print(result)
         print('===================')
@@ -112,6 +135,11 @@ def parse_query(raw):
                     top = op.pop()
             elif r not in OPWORDS:
                 output.append(r)
+                # case: `NOT` is immediately followed by an operand
+                if len(op) != 0 and op[-1] == 'NOT':
+                    output.append(op.pop())
+            elif r == 'NOT':
+                op.append(r)
             else:
                 if len(op) != 0:
                     top = op.pop()
@@ -127,6 +155,7 @@ def parse_query(raw):
     while(len(op) != 0):
         output.append(op.pop())
 
+    print('RPN:'+str(output))
     return evaluate_query(output)
 
 
@@ -148,11 +177,9 @@ def evaluate_query(qlist):
         if q in OPWORDS:
             if q == 'NOT':
                 op1 = stack.pop()
-                # if op1 is a list: it is an intermediate result from previosu steps
-                # else: op1 is a keyword, retrieve corresponding postings list
-                print('plist:'+str(plist))
-                plist = op1 if type(op1)==list else get_posting(op1)
-                stack.append(evaluate_NOT(plist))
+                print('plist:'+str(op1))
+                print('doclist:'+str(doclist))
+                stack.append(evaluate_NOT(op1))
             else:
                 op1 = stack.pop()
                 op2 = stack.pop()
@@ -165,7 +192,16 @@ def evaluate_query(qlist):
                 elif q == 'OR':
                     stack.append(evaluate_OR(plist1, plist2))
         else:
-            stack.append(q)
+            # operator, get postings from dictionary 
+            q = porter.stem(q.lower())
+            print(q)
+            plist = [] if q not in postings else postings[q]
+            if q in postings:
+                print('Retrieving posting for '+q+':' + str(postings[q]))
+            else:
+                print('No record for '+q)
+            print('==============')
+            stack.append(plist)
     return stack.pop()
 
 
@@ -194,18 +230,23 @@ if input_directory == None or output_file_postings == None or output_file_dictio
 
 start_time = time.time()
 # in-memory temporary posting list
-p = {} 
+postings = {} 
 # using porter stemmer
 porter = nltk.PorterStemmer()
 # get the list of files inside the input directory
 files = glob.glob(input_directory + "*")
+# list of indexed documents 
+doclist = []
 fcount = 0
 for f in files:
     docID = f.split('/')[-1]
     # TESTING ONLY
     fcount += 1
-    if fcount > 100:
+    if fcount > 10:
         break
+
+    doclist.append(docID)
+    
 
     with open(f, 'r') as fopen:
         for sent in nltk.sent_tokenize(fopen.read()):
@@ -213,14 +254,22 @@ for f in files:
             for t in tokens:
                 t = porter.stem(t.lower()).encode("ascii")
                 # create a new dictionary entry if t is a new keyword
-                if t not in p:
-                    p[t] = [docID]
+                if t not in postings:
+                    postings[t] = [docID]
                 else:
                     # add docID to posting list if it is not inside
-                    if docID not in p[t]:
-                        p[t].append(docID)
+                    if docID not in postings[t]:
+                        postings[t].append(docID)
+
+    # sort docIDs in posting list
+    for key in postings:
+        postings[key].sort()
+    # sort docID list
+    doclist.sort()
 
 print(str(fcount)+" records processed in "+str(time.time() - start_time)+" seconds.")
 #query = "bill OR Gates AND (vista OR XP) AND NOT mac"
-query = "bill AND Gates"
+print('Indexed docID:'+str(doclist))
+print(postings)
+query = "as OR NOT again"
 print(parse_query(query))
