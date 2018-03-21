f423f376b941d030c16eb788f25590dc2ee822d2
senyuuri <senyuuri@gmail.com>
2018-03-21 13:41:22 +0800

    Add in-memory lnc.ltc indexing

--- hw3/index.py
+++ hw3/index.py
@@ -8,6 +8,9 @@ import time
 import pickle
 import math
 
+
+DEBUG = True
+
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
 
@@ -43,14 +46,18 @@ porter = nltk.PorterStemmer()
 files = glob.glob(input_directory + "*")
 # list of indexed documents 
 doclist = []
+# list of document length for cosine normalisation
+lenlist = []
 fcount = 0
 
 for f in files:
-    docID = f.split('/')[-1]
+    docID = int(f.split('/')[-1])
     # TESTING ONLY
     fcount += 1
-    # if fcount > 1000:
-    #     break
+    if DEBUG:
+        if fcount > 2:
+            break
+
     if fcount % 1000 == 0:
         print(str(fcount)+' files processed......')
     #     break
@@ -58,67 +65,109 @@ for f in files:
     
     # index each file
     with open(f, 'r') as fopen:
+        # terms in each document, for later tf-idf calculation
+        termlist = []
+
         for sent in nltk.sent_tokenize(fopen.read()):
             # remove common punctuations
             sent = sent.replace(',',' ').replace('.',' ').replace('\'',' ')
             tokens = nltk.word_tokenize(sent)
             for t in tokens:
                 t = porter.stem(t.lower()).encode("ascii")
+                termlist.append(t)
                 # create a new dictionary entry if t is a new keyword
                 if t not in postings:
-                    postings[t] = [docID]
+                    postings[t] = [[docID, 1]]
                 else:
-                    # add docID to posting list if it is not inside
-                    if docID not in postings[t]:
-                        postings[t].append(docID)
+                    found = False
+                    for plist in postings[t]:
+                        if plist[0] == docID:
+                            # add term frequencuy
+                            plist[1] += 1
+                            found = True
+                            break
+                    # add docID to posting list if it is not already inside
+                    if not found: 
+                        postings[t].append([docID, 1])
 
-# sort docIDs in posting list
-for key in postings:
-    postings[key] = [int(x) for x in postings[key]]
-    postings[key].sort()
+        # calculate document length
+        # remove duplicate terms
+        termset = set(termlist)
+        tf_log_sum = 0 
+        for t in termset:
+            # get term frequency from postings
+            # assertion: t in postings 
+            # must be true since all terms are added to the postings list in the previous loop
+            for plist in postings[t]:
+                if plist[0] == docID:
+                    freq = plist[1]
 
-# sort docID list
-doclist = [int(x) for x in doclist]
-doclist.sort()
+            print(t, freq, 1+math.log(freq, 10),tf_log_sum)
+            # bulletproof code. This should never happen!
+            if freq == None:
+                raise LookupError("Term '" + t + "'not found in the postings list. Not good:(")
+                sys.exit(2)
+
+            # normalise tf and add together
+            tf_log_sum += (1 + math.log(freq, 10))
+        lenlist.append(round(math.sqrt(tf_log_sum), 2))
+
+if DEBUG:
+    print("In-memory postings list:")
+    print("=================================================")
+    for term, plist in postings.items():
+        print(term, len(plist) ,plist) 
+
+    print("doclist:", doclist[:10])
+    print("lenlist:",lenlist[:10])
+
+# # sort docIDs in posting list
+# for key in postings:
+#     postings[key] = [int(x) for x in postings[key]]
+#     postings[key].sort()
+
+# # sort docID list
+# doclist = [int(x) for x in doclist]
+# doclist.sort()
 
 print(str(fcount)+" records processed in "+str(time.time() - start_time)+" seconds.")
 
 
 # dictionary to be saved 
-dictionary = {}
+# dictionary = {}
 
 # save postings list to file
-with open(output_file_postings, 'w') as fp:
-    for key, p_list in postings.items():
-        # get initial write cursor position
-        w_cursor = fp.tell()
-        # add skip pointers
-        skip_list = []
-        interval = int(math.floor(math.sqrt(len(p_list))))
-        for i in range(len(p_list)):
-            # this is a node with skip pointer
-            if (i % interval == 0) and (i != len(p_list) - 1):
+# with open(output_file_postings, 'w') as fp:
+#     for key, p_list in postings.items():
+#         # get initial write cursor position
+#         w_cursor = fp.tell()
+#         # add skip pointers
+#         skip_list = []
+#         interval = int(math.floor(math.sqrt(len(p_list))))
+#         for i in range(len(p_list)):
+#             # this is a node with skip pointer
+#             if (i % interval == 0) and (i != len(p_list) - 1):
 
-                # add special tag '#' to indicate that the following node is a skip pointer
-                skip_list.append('#'+str(p_list[i]))
-                # docID in which the skip pointer is pointing to  
-                skip_index = (i + interval) if (i + interval < len(p_list)) else (len(p_list) - 1)
-                doc_id = p_list[skip_index]
-                # offset to the skip destination
-                offset = interval
-                # skip pointer format: !docID!offset 
-                skip_list.append('!'+str(doc_id)+'!'+str(offset))
-            else:
-                # normal node
-                skip_list.append(str(p_list[i]))
+#                 # add special tag '#' to indicate that the following node is a skip pointer
+#                 skip_list.append('#'+str(p_list[i]))
+#                 # docID in which the skip pointer is pointing to  
+#                 skip_index = (i + interval) if (i + interval < len(p_list)) else (len(p_list) - 1)
+#                 doc_id = p_list[skip_index]
+#                 # offset to the skip destination
+#                 offset = interval
+#                 # skip pointer format: !docID!offset 
+#                 skip_list.append('!'+str(doc_id)+'!'+str(offset))
+#             else:
+#                 # normal node
+#                 skip_list.append(str(p_list[i]))
 
-        fp.write(','.join(skip_list))
-        fp.write('\n')
-        dictionary[key] = [len(p_list), w_cursor]
+#         fp.write(','.join(skip_list))
+#         fp.write('\n')
+#         dictionary[key] = [len(p_list), w_cursor]
 
-# save dictionary to file
-with open(output_file_dictionary, 'w') as fd:
-    # write the list of docIDs in the first line
-    fd.write(','.join([str(x) for x in doclist]) + '\n')
-    for key, value in dictionary.items():
-        fd.write(key + ',' + str(value[0]) + ',' + str(value[1]) + '\n')
+# # save dictionary to file
+# with open(output_file_dictionary, 'w') as fd:
+#     # write the list of docIDs in the first line
+#     fd.write(','.join([str(x) for x in doclist]) + '\n')
+#     for key, value in dictionary.items():
+#         fd.write(key + ',' + str(value[0]) + ',' + str(value[1]) + '\n')
